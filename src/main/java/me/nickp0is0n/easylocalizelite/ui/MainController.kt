d544eb76ca9735935cef0b73a239e1c1deccd02e
Mykola Chaikovskyi <nickchaywork@gmail.com>
2021-11-22 22:28:10 +0200

    Added save project functionality

--- src/main/java/me/nickp0is0n/easylocalizelite/ui/MainController.kt
+++ src/main/java/me/nickp0is0n/easylocalizelite/ui/MainController.kt
@@ -1,5 +1,9 @@
 package me.nickp0is0n.easylocalizelite.ui
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import me.nickp0is0n.easylocalizelite.models.LocalizedString
 import me.nickp0is0n.easylocalizelite.models.ParserSettings
 import me.nickp0is0n.easylocalizelite.models.StringIDListModel
@@ -7,10 +11,7 @@ import me.nickp0is0n.easylocalizelite.utils.AppInfo
 import me.nickp0is0n.easylocalizelite.utils.LocalizeParser
 import java.awt.FileDialog
 import java.awt.event.ActionEvent
-import java.io.File
-import java.io.FileInputStream
-import java.io.IOException
-import java.io.ObjectInputStream
+import java.io.*
 
 class MainController(val form: MainForm) {
     val list = mutableListOf(LocalizedString("No file is currently loaded.", "", ""))
@@ -28,18 +29,8 @@ class MainController(val form: MainForm) {
         }
 
         form.setOpenMenuItemOnClickListener(onOpenItemClick)
+        form.setSaveAsMenuItemOnClickListener(onSaveAsItemClick)
         form.setStringAreaOnEditListener(onStringEdit)
-    }
-
-    val onStringEdit = fun() {
-        val currentString = list[form.stringIDListCurrentSelectionIndex]
-        list[form.stringIDListCurrentSelectionIndex] = LocalizedString(
-            currentString.id,
-            form.stringAreaText,
-            currentString.comment,
-            mark = currentString.mark,
-            copyrightHeader = currentString.copyrightHeader
-        )
     }
 
     val onOpenItemClick = fun(_: ActionEvent) {
@@ -51,20 +42,62 @@ class MainController(val form: MainForm) {
                 it.clear()
             }.add(LocalizedString("No file is currently loaded.", "", ""))
         }
-        form.setTitle(AppInfo.windowTitle + " – " + openDialog.files[0].name)
-        if (openDialog.files[0].extension == "elproject") {
-            currentSaveFile = openDialog.files[0]
-            ObjectInputStream(FileInputStream(currentSaveFile!!)).use {
+        else {
+            form.setTitle(AppInfo.windowTitle + " – " + openDialog.files[0].name)
+            if (openDialog.files[0].extension == "elproject") {
+                currentSaveFile = openDialog.files[0]
+                ObjectInputStream(FileInputStream(currentSaveFile!!)).use {
+                    list.also {
+                        it.clear()
+                    }.addAll(it.readObject() as List<LocalizedString>)
+                }
+            }
+            else {
+                val stringFile = openDialog.files[0]
                 list.also {
                     it.clear()
-                }.addAll(it.readObject() as List<LocalizedString>)
+                }.addAll(try { parser.fromFile(stringFile) } catch (e: IOException) { listOf(LocalizedString("No file loaded", "", ""))})
             }
         }
-
-        val stringFile = openDialog.files[0]
-        list.also {
-            it.clear()
-        }.addAll(try { parser.fromFile(stringFile) } catch (e: IOException) { listOf(LocalizedString("No file loaded", "", ""))})
         model.setElements(list)
+    }
+
+    val onSaveAsItemClick = fun(_: ActionEvent) {
+        val saveDialog = FileDialog(form)
+        saveDialog.mode = FileDialog.SAVE
+        saveDialog.file = "*.elproject"
+        saveDialog.isVisible = true
+        if (saveDialog.files.isNotEmpty()) {
+            currentSaveFile = saveDialog.files[0]
+            saveProjectFile()
+        }
+    }
+
+    private fun saveProjectFile() {
+        CoroutineScope(Dispatchers.IO).launch {
+            writeToProjectFile()
+        }
+    }
+
+    private suspend fun writeToProjectFile() = withContext(Dispatchers.IO) {
+        try {
+            ObjectOutputStream(FileOutputStream(currentSaveFile!!)).use {
+                it.writeObject(list)
+            }
+        }
+        catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    val onStringEdit = fun() {
+        val currentString = list[form.stringIDListCurrentSelectionIndex]
+        list[form.stringIDListCurrentSelectionIndex] = LocalizedString(
+            currentString.id,
+            form.stringAreaText,
+            currentString.comment,
+            mark = currentString.mark,
+            copyrightHeader = currentString.copyrightHeader
+        )
     }
 }
